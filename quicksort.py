11a5b4a1b84eefdd2e5702adc7cbcd79d15e4497
Edmund Martin <edmartin101@googlemail.com>
2017-05-12 21:58:42 +0100

    Create quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,20 @@
+def quicksort(array):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        return quicksort(less) + equal + quicksort(greater)
+    else:
+        return array
+
+a = quicksort([1,29,287,276,19,7])
+print(a)
